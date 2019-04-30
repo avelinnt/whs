cbff986e4604d97007fcdf68425515da7e73f568
avelinnt <guiloizeau.valentin@gmail.com>
2019-04-30 09:13:25 +0200

    ajout des routes put et delete dans route.php

--- serveur/src/routes.php
+++ serveur/src/routes.php
@@ -27,25 +27,17 @@ $app->get('/API/assertions', function(Request $request, Response $response) {
 
 });
 
-$app->post('/API/assertions', function(Request $request, Response $response) {
+$app->post('/API/assertions', function (Request $request, Response $response, $arg) {
 
   $body = $request->getBody();
-  //true pour retourner un tableau associatif
-  $input = json_decode($body, true);
+  $decode_body = json_decode($body, true);
 
-  $result = App\Assertion::insert(
-    ['topic' =>  $topic = $input["topic"], 'sentence' =>  $sentence = $input["sentence"]]
-  );
-
-  // A REVOIR EN DESSOUS
-
-  $assertions = json_encode($result);
-  $body = $response->getBody();
-  $body->write($assertions);
-  $newResponse = $response->withHeader('Content-type', 'application/json');
-  return $newResponse;
-
-
+  $assertion = new App\Assertion;
+  $assertion -> topic = $decode_body['topic'];
+  $assertion -> sentence = $decode_body['sentence'];
+  $assertion -> save();
+  $result = json_encode($assertion);
+  return $result;
 });
 
 $app->get('/API/assertions/{id}', function(Request $request, Response $response) {
@@ -53,6 +45,40 @@ $app->get('/API/assertions/{id}', function(Request $request, Response $response)
   $route = $request->getAttribute('route');
   $assId = $route->getArgument('id');
   $result = App\Assertion::find($assId);
+
+  $assertions = json_encode($result);
+  $body = $response->getBody();
+  $body->write($assertions);
+  $newResponse = $response->withHeader('Content-type', 'application/json');
+  return $newResponse;
+
+});
+
+$app->put('/API/assertions/{id}', function(Request $request, Response $response) {
+  $body = $request->getBody();
+  $decode_body = json_decode($body, true);
+  $route = $request->getAttribute('route');
+  $assId = $route->getArgument('id');
+  $result = App\Assertion::find($assId);
+  $result -> topic = $decode_body['topic'];
+  $result -> sentence = $decode_body['sentence'];
+  $result->save();
+
+  $assertions = json_encode($result);
+  $body = $response->getBody();
+  $body->write($assertions);
+  $newResponse = $response->withHeader('Content-type', 'application/json');
+  return $newResponse;
+
+});
+
+$app->delete('/API/assertions/{id}', function(Request $request, Response $response) {
+  $body = $request->getBody();
+  $decode_body = json_decode($body, true);
+  $route = $request->getAttribute('route');
+  $assId = $route->getArgument('id');
+  $result = App\Assertion::find($assId);
+  $result -> delete();
 
   $assertions = json_encode($result);
   $body = $response->getBody();
